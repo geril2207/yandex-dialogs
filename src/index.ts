a2e0763c6daf84b50b8cf037e40c2407ddc44aae
Gergert Ilya <gergert.ilya@mail.ru>
2022-12-08 10:37:33 +0300

    move data to response field

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,15 @@ app.use(bodyParser.json())
 app.post('/', (req, res) => {
   console.log('req :>> ', req)
   const { version, session } = req.body
-  const responseData: { text: string; version?: any; session?: any } = {
-    text: 'Привет я Тестовая Алиса',
-  }
-  if (version) responseData.version = version
-  if (version) responseData.session = session
 
-  res.json({ response: responseData })
+  res.json({
+    version,
+    session,
+    response: {
+      text: 'Привет, Я Алиса, а ты?',
+      end_session: false,
+    },
+  })
 })
 
 app.listen(port, () => {
