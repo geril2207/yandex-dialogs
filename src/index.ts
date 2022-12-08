c3f9105ca9c6b86e7e851822c0de89c034fb770e
Gergert Ilya <gergert.ilya@mail.ru>
2022-12-08 10:31:14 +0300

    change body response

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,14 @@ app.use(bodyParser.json())
 
 app.post('/', (req, res) => {
   console.log('req :>> ', req)
-  res.json('Привет я Тестовая Алиса')
+  const { version, session } = req.body
+  const responseData: { text: string; version?: any; session?: any } = {
+    text: 'Привет я Тестовая Алиса',
+  }
+  if (version) responseData.version = version
+  if (version) responseData.session = session
+
+  res.json(responseData)
 })
 
 app.listen(port, () => {
